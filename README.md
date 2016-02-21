33624ffb406d78d49d830edbbb4eadf481a1efea
Marko Korhonen <marko.korhonen@gmail.com>
2016-02-21 18:36:09 +0200

    implementations

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# php-mongodb-crud
+# back-2-95/fields-mongodb
+
 CRUD library for storing stuff to MongoDB with PHP7
 
 Testing... Testing Packagist auto-update....
@@ -6,4 +7,14 @@ Testing... Testing Packagist auto-update....
 ## What should this library do? ##
 
 - Use MongoDb for storing Entities handled by \BackTo95\Fields API (create own storage adapter for it)
-- 
+
+## API
+
+Create API and set MongoDB storage:
+
+```PHP
+$api = new \BackTo95\Fields\Api();
+$client = new \MongoDB\Client;
+$storage = new \BackTo95\Fields\MongoDb\MongoDbStorage($client);
+$api->setStorage($storage);
+```
--- src/MongoDbStorage.php
+++ src/MongoDbStorage.php
@@ -4,16 +4,37 @@ namespace BackTo95\Fields\MongoDb;
 
 use BackTo95\Fields\Entity\EntityConfiguration;
 use BackTo95\Fields\Storage\StorageInterface;
+use MongoDB\Client;
+use MongoDB\Collection;
 
 class MongoDbStorage implements StorageInterface
 {
+    protected $mongodb;
+
+    public function __construct(Client $mongodb)
+    {
+        $this->mongodb = $mongodb;
+    }
+
     public function getEntityConfiguration(string $entity) : EntityConfiguration
     {
-        // TODO: Implement getEntityConfiguration() method.
+        $configuration = $this->getCollection()->findOne(['entity' => $entity]);
+
+        return new EntityConfiguration($configuration);
+    }
+
+    protected function getCollection() : Collection
+    {
+        return $this->mongodb->selectCollection('demo', 'entity_configurations');
     }
 
     public function storeEntityConfiguration(EntityConfiguration $configuration) : bool
     {
-        // TODO: Implement storeEntityConfiguration() method.
+        $entity = $configuration->getName();
+        $data = $configuration->getArrayCopy();
+
+        $result = $this->getCollection()->updateOne(['entity' => $entity], $data);
+
+        return ($result->getModifiedCount() === 1);
     }
 }
