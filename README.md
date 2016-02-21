9f6f5df498c9d0fc1d2217b6ba1d1a5e7a33f54f
Marko Korhonen <marko.korhonen@gmail.com>
2016-02-21 18:01:51 +0200

    uuh, changes.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 CRUD library for storing stuff to MongoDB with PHP7
 
 Testing... Testing Packagist auto-update....
+
+## What should this library do? ##
+
+- Use MongoDb for storing Entities handled by \BackTo95\Fields API (create own storage adapter for it)
+- 
---
+++ src/MongoDbStorage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BackTo95\Fields\MongoDb;
+
+use BackTo95\Fields\Entity\EntityConfiguration;
+use BackTo95\Fields\Storage\StorageInterface;
+
+class MongoDbStorage implements StorageInterface
+{
+    public function getEntityConfiguration(string $entity) : EntityConfiguration
+    {
+        // TODO: Implement getEntityConfiguration() method.
+    }
+
+    public function storeEntityConfiguration(EntityConfiguration $configuration) : bool
+    {
+        // TODO: Implement storeEntityConfiguration() method.
+    }
+}
